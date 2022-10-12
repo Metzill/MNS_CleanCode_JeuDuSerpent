b527393749762fa064bec6f76a8e7fc7cfb1fd60
KREMER Hugo <33060853+Metzill@users.noreply.github.com>
2022-10-12 10:33:35 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Dice
+    {
+        private readonly Random random = new Random();
+
+        public int RollDice()
+        {
+            return random.Next(6);
+        }
+    }
+}
